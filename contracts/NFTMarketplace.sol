1bd4f00d7198f9bbaa032a564c27eee9c5f94d53
fangjun <fjun99@gmail.com>
2022-02-28 23:10:24 +0800

    prepare script

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -34,7 +34,21 @@ contract NFTMarketplace is ReentrancyGuard {
 
   mapping(uint256 => MarketItem) private marketItems;
 
+  /**
+   * @dev 
+   * todo sale event
+   */
   event MarketItemCreated (
+    uint indexed id,
+    address indexed nftContract,
+    uint256 indexed tokenId,
+    address seller,
+    address buyer,
+    uint256 price,
+    State state
+  );
+
+  event MarketItemSaled (
     uint indexed id,
     address indexed nftContract,
     uint256 indexed tokenId,
@@ -106,7 +120,7 @@ contract NFTMarketplace is ReentrancyGuard {
   function deleteMarketItem(uint256 itemId) public nonReentrant {
 
     require(itemId <= _itemCounter.current(), "id must <= item count");
-    MarketItem memory item = marketItems[itemId];
+    MarketItem storage item = marketItems[itemId];
 
     require(IERC721(item.nftContract).ownerOf(item.tokenId) == msg.sender, "must be the owner");
     require(IERC721(item.nftContract).getApproved(item.tokenId) == address(this), "NFT must be approved to market");
@@ -116,9 +130,21 @@ contract NFTMarketplace is ReentrancyGuard {
               abi.encodeWithSignature("approve(address,uint256)",address(0), item.tokenId)
             );
 
-    marketItems[itemId].state = State.Inactive;
+    item.state = State.Inactive;
 
     require(!success,"delegate call ");  
+
+    emit MarketItemSaled(
+      itemId,
+      item.nftContract,
+      item.tokenId,
+      item.seller,
+      address(0),
+      0,
+      State.Inactive
+    );
+
+    console.log("item.state",2);
 
     //https://docs.soliditylang.org/en/v0.8.12/control-structures.html
     //The low-level functions call, delegatecall and staticcall return true as their first return value 
@@ -153,6 +179,16 @@ contract NFTMarketplace is ReentrancyGuard {
     item.buyer = payable(msg.sender);
     item.state = State.Release;
     _itemSoldCounter.increment();    
+
+    emit MarketItemSaled(
+      id,
+      nftContract,
+      tokenId,
+      item.seller,
+      msg.sender,
+      price,
+      State.Release
+    );    
   }
 
   /**
@@ -168,6 +204,7 @@ contract NFTMarketplace is ReentrancyGuard {
 
   /**
    * @dev Returns only market items a user has purchased
+   * todo pagination
    */
   function fetchMyPurchasedItems() public view returns (MarketItem[] memory) {
     return fetchHepler(FetchOperator.MyPurchasedItems);
@@ -175,7 +212,8 @@ contract NFTMarketplace is ReentrancyGuard {
 
   /**
    * @dev Returns only market items a user has created
-   */
+   * todo pagination
+  */
   function fetchMyCreatedItems() public view returns (MarketItem[] memory) {
     return fetchHepler(FetchOperator.MyCreatedItems);
   }
@@ -184,6 +222,7 @@ contract NFTMarketplace is ReentrancyGuard {
 
   /**
    * @dev fetch helper
+   * todo pagination   
    */
    function fetchHepler(FetchOperator _op) private view returns (MarketItem[] memory) {     
     uint total = _itemCounter.current();
@@ -214,9 +253,14 @@ contract NFTMarketplace is ReentrancyGuard {
    */
   function isCondition(MarketItem memory item, FetchOperator _op) private view returns (bool){
     if(_op == FetchOperator.MyCreatedItems){ 
-      return item.seller == msg.sender ? true: false;
+      return 
+        (item.seller == msg.sender
+          && item.state != State.Inactive
+        )? true
+         : false;
     }else if(_op == FetchOperator.MyPurchasedItems){
-      return item.buyer ==  msg.sender ? true: false;
+      return
+        (item.buyer ==  msg.sender) ? true: false;
     }else if(_op == FetchOperator.ActiveItems){
       return 
         (item.buyer == address(0) 
---
+++ src/prepare.ts
@@ -0,0 +1,85 @@
+import { Signer } from "ethers"
+import { ethers } from "hardhat"
+import { BadgeToken, NFTMarketplace } from  "../typechain"
+import { tokenAddress, marketAddress } from "./constants"
+const base64 = require( "base-64")
+
+const _name='BadgeToken'
+const _symbol='BADGE'
+
+async function main() {
+
+  console.log("========   deploy to a **new** localhost ======")
+
+  /* deploy the NFT contract */
+  const NFT = await ethers.getContractFactory("BadgeToken")
+  const nftContract:BadgeToken = await NFT.deploy(_name,_symbol)
+  await nftContract.deployed()
+
+  /* deploy the marketplace */
+  const Market = await ethers.getContractFactory("NFTMarketplace")
+  const marketContract:NFTMarketplace = await Market.deploy()
+  await marketContract.deployed()
+  
+  console.log("nftContractAddress:",nftContract.address)
+  console.log("marketAddress     :",marketContract.address)
+
+  console.log("========   Prepare for webapp dev ======")
+  console.log("nftContractAddress:",tokenAddress)
+  console.log("marketAddress     :",marketAddress)
+  console.log("**should be the same**")
+
+  let owner:Signer,account1:Signer,account2:Signer
+  
+  [owner, account1,account2] = await ethers.getSigners()
+  const address0 = await owner.getAddress()
+  const address1 = await account1.getAddress()
+  const address2 = await account2.getAddress()
+
+  const market:NFTMarketplace = await ethers.getContractAt("NFTMarketplace", marketAddress)
+  const nft:BadgeToken = await ethers.getContractAt("BadgeToken", tokenAddress)
+
+  const listingFee = await market.getListingFee()
+  const auctionPrice = ethers.utils.parseUnits('1', 'ether')
+
+  console.log("1. == mint 1-6 to account#0")
+  for(let i=1;i<=6;i++){
+    await nft.mintTo(address0)
+  }
+
+  console.log("2. == list 1-6 to market")
+  for(let i=1;i<=6;i++){
+    await nft.approve(marketAddress,i)
+    await market.createMarketItem(tokenAddress, i, auctionPrice, { value: listingFee })
+  }
+
+  console.log("3. == mint 7-9 to account#0")
+  for(let i=7;i<=9;i++){
+    await nft.connect(account1).mintTo(address1)
+  }
+
+  console.log("4. == list 1-6 to market")
+  for(let i=7;i<=9;i++){
+    await nft.connect(account1).approve(marketAddress,i)
+    await market.connect(account1).createMarketItem(tokenAddress, i, auctionPrice, { value: listingFee })
+  }
+
+  console.log("5. == account#0 buy 7 & 8")
+  await market.createMarketSale(tokenAddress, 7, { value: auctionPrice})
+  await market.createMarketSale(tokenAddress, 8, { value: auctionPrice})
+
+  console.log("6. == account#1 buy 1")
+  await market.connect(account1).createMarketSale(tokenAddress, 1, { value: auctionPrice})
+
+  console.log("7. == account#2 buy 2")
+  await market.connect(account2).createMarketSale(tokenAddress, 2, { value: auctionPrice})
+
+  console.log("8. == account#0 delete 6")
+  await market.deleteMarketItem(6)
+
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
